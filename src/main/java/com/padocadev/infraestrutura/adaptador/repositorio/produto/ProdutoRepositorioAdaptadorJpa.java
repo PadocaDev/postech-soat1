4db31c22517953f3c61a0bd3ae1e82b705e0f38a
Sonia A. Ribeiro <sonia.ribeiro@alumni.usp.br>
2023-06-17 13:34:53 +0100

    Cria casos de uso e beans de configuração

---
+++ src/main/java/com/padocadev/infraestrutura/adaptador/repositorio/produto/ProdutoRepositorioAdaptadorJpa.java
@@ -0,0 +1,72 @@
+package com.padocadev.infraestrutura.adaptador.repositorio.produto;
+
+import com.padocadev.dominio.entidade.produto.Produto;
+import com.padocadev.dominio.excecao.produto.ProdutoNaoExisteExcecao;
+import com.padocadev.dominio.porta.produto.ProdutoRepositorioAdaptadorPorta;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class ProdutoRepositorioAdaptadorJpa implements ProdutoRepositorioAdaptadorPorta {
+
+    private final ProdutoRepositorioJpa produtoRepositorioJpa;
+
+    public ProdutoRepositorioAdaptadorJpa(ProdutoRepositorioJpa produtoRepositorioJpa) {
+        this.produtoRepositorioJpa = produtoRepositorioJpa;
+    }
+
+    @Override
+    public boolean existeProdutoComNome(String nome) {
+        return produtoRepositorioJpa.existsByNome(nome);
+    }
+
+    @Override
+    public Produto criarProduto(Produto produto) {
+        ProdutoEntidadeJpa produtoEntidadeJpa = new ProdutoEntidadeJpa(produto.getDataCadastro(), produto.getNome(), produto.getCategoria(), produto.getPreco());
+        produtoRepositorioJpa.save(produtoEntidadeJpa);
+
+        produto.setId(produtoEntidadeJpa.getId());
+        return produto;
+    }
+
+    @Override
+    public Optional<Produto> buscaPorId(Long produtoId) {
+        return produtoRepositorioJpa.findById(produtoId)
+                .flatMap(produtoEntidadeJpa -> Optional.of(
+                            new Produto(
+                                    produtoEntidadeJpa.getId(),
+                                    produtoEntidadeJpa.getDataCadastro(),
+                                    produtoEntidadeJpa.getNome(),
+                                    produtoEntidadeJpa.getCategoria(),
+                                    produtoEntidadeJpa.getPreco()
+                            )
+                )
+                );
+    }
+
+    @Override
+    public Optional<Produto> buscaPorNome(String nome) {
+        return produtoRepositorioJpa.findByNome(nome)
+                .flatMap(produtoEntidadeJpa -> Optional.of(
+                                new Produto(
+                                        produtoEntidadeJpa.getId(),
+                                        produtoEntidadeJpa.getDataCadastro(),
+                                        produtoEntidadeJpa.getNome(),
+                                        produtoEntidadeJpa.getCategoria(),
+                                        produtoEntidadeJpa.getPreco()
+                                )
+                        )
+                );
+    }
+
+    @Override
+    public Produto editarProduto(Long produtoId, Produto produtoParaEditar) {
+        ProdutoEntidadeJpa produtoExistenteEntidadeJpa = produtoRepositorioJpa.findById(produtoId).orElseThrow(ProdutoNaoExisteExcecao::new);
+        produtoParaEditar.atualiza(produtoExistenteEntidadeJpa);
+        produtoRepositorioJpa.save(produtoExistenteEntidadeJpa);
+
+        produtoParaEditar.setId(produtoExistenteEntidadeJpa.getId());
+        return produtoParaEditar;
+    }
+}
--- src/main/java/com/padocadev/infraestrutura/configuracao/BeanConfiguracao.java
+++ src/main/java/com/padocadev/infraestrutura/configuracao/BeanConfiguracao.java
@@ -1,10 +1,13 @@
 package com.padocadev.infraestrutura.configuracao;
 
-import com.padocadev.infraestrutura.adaptador.repositorio.cliente.ClienteRepositorioAdaptadorJpa;
 import com.padocadev.dominio.casodeuso.cliente.BuscaClientePorCpfCasoDeUso;
 import com.padocadev.dominio.casodeuso.cliente.CriaClienteCasoDeUso;
+import com.padocadev.dominio.casodeuso.produto.*;
 import com.padocadev.dominio.porta.cliente.BuscaClientePorCpfCasoDeUsoPorta;
 import com.padocadev.dominio.porta.cliente.CriaClienteCasoDeUsoPorta;
+import com.padocadev.dominio.porta.produto.*;
+import com.padocadev.infraestrutura.adaptador.repositorio.cliente.ClienteRepositorioAdaptadorJpa;
+import com.padocadev.infraestrutura.adaptador.repositorio.produto.ProdutoRepositorioAdaptadorJpa;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,5 +22,20 @@ public class BeanConfiguracao {
     @Bean
     BuscaClientePorCpfCasoDeUsoPorta buscaClientePorCpfCasoDeUsoPorta(ClienteRepositorioAdaptadorJpa clienteRepositorioAdaptadorJpa) {
         return new BuscaClientePorCpfCasoDeUso(clienteRepositorioAdaptadorJpa);
+    }
+
+    @Bean
+    CriaProdutoCasoDeUsoPorta criaProdutoCasoDeUsoPorta(ProdutoRepositorioAdaptadorJpa produtoRepositorioAdaptadorJpa) {
+        return new CriaProdutoCasoDeUso(produtoRepositorioAdaptadorJpa);
+    }
+
+    @Bean
+    BuscaProdutoPorIdCasoDeUsoPorta buscaProdutoPorIdCasoDeUsoPorta(ProdutoRepositorioAdaptadorJpa produtoRepositorioAdaptadorJpa) {
+        return new BuscaProdutoPorIdCasoDeUso(produtoRepositorioAdaptadorJpa);
+    }
+
+    @Bean
+    EditaProdutoCasoDeUsoPorta editaProdutoCasoDeUsoPorta(ProdutoRepositorioAdaptadorJpa produtoRepositorioAdaptadorJpa) {
+        return new EditaProdutoCasoDeUso(produtoRepositorioAdaptadorJpa);
     }
 }
