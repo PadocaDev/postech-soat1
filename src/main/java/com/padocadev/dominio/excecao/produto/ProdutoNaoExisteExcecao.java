59a1e4a7feb30749bc539dd8950654bf1cf5cf26
Sonia A. Ribeiro <sonia.ribeiro@alumni.usp.br>
2023-06-17 15:11:19 +0100

    Cria testes para controlador

--- src/main/java/com/padocadev/dominio/excecao/produto/ProdutoNaoExisteExcecao.java
+++ src/main/java/com/padocadev/dominio/excecao/produto/ProdutoNaoExisteExcecao.java
@@ -3,6 +3,6 @@ package com.padocadev.dominio.excecao.produto;
 public class ProdutoNaoExisteExcecao extends RuntimeException {
 
     public ProdutoNaoExisteExcecao() {
-        super("Produto com o nome informado não existe!");
+        super("Produto com o id informado não existe!");
     }
 }
