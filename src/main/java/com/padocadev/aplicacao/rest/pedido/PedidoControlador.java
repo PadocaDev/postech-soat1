a75e8cd356e60d4eb0e8e321b56eb7f95d3d1df6
Vitor Marcato <vitor.marcato@alura.com.br>
2023-06-20 21:02:29 -0300

    adiciona controller

--- src/main/java/com/padocadev/aplicacao/rest/pedido/PedidoControlador.java
+++ src/main/java/com/padocadev/aplicacao/rest/pedido/PedidoControlador.java
@@ -1,34 +1,42 @@
 package com.padocadev.aplicacao.rest.pedido;
 
 import com.padocadev.aplicacao.requisicao.pedido.PedidoRequisicao;
+import com.padocadev.aplicacao.resposta.PedidoResposta;
 import com.padocadev.dominio.entidade.pedido.Pedido;
+import com.padocadev.dominio.porta.pedido.BuscaTodosPedidosCasoDeUsoPorta;
 import com.padocadev.dominio.porta.pedido.CriaPedidoCasoDeUsoPorta;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/pedidos")
 public class PedidoControlador {
 
     private final CriaPedidoCasoDeUsoPorta criaPedidoCasoDeUso;
+    private final BuscaTodosPedidosCasoDeUsoPorta buscaTodosPedidosCasoDeUso;
 
-    public PedidoControlador(CriaPedidoCasoDeUsoPorta criaPedidoCasoDeUso) {
+    public PedidoControlador(CriaPedidoCasoDeUsoPorta criaPedidoCasoDeUso, BuscaTodosPedidosCasoDeUsoPorta buscaTodosPedidosCasoDeUso) {
         this.criaPedidoCasoDeUso = criaPedidoCasoDeUso;
+        this.buscaTodosPedidosCasoDeUso = buscaTodosPedidosCasoDeUso;
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<PedidoResposta>> buscaTodosPedidos() {
+        List<Pedido> pedidos = buscaTodosPedidosCasoDeUso.buscarTodosPedidos();
+        return ResponseEntity.ok(PedidoResposta.listarPedidos(pedidos));
     }
 
     @PostMapping
-    public ResponseEntity criaPedido(@RequestBody @Valid PedidoRequisicao pedidoRequisicao) {
-
-        Pedido pedidoRealizado = new Pedido(pedidoRequisicao.produtos(), pedidoRequisicao.getClienteId());
-        Pedido pedidoCriado = criaPedidoCasoDeUso.criar(pedidoRealizado);
-
-        return ResponseEntity.ok().build();
+    public ResponseEntity<PedidoResposta> criaPedido(@RequestBody @Valid PedidoRequisicao pedidoRequisicao) {
+        Pedido pedidoCriado = criaPedidoCasoDeUso.criar(pedidoRequisicao.converteParaPedido());
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{numeroPedido}")
+                .buildAndExpand(pedidoCriado.getNumeroPedido())
+                .toUri();
+        return ResponseEntity.created(location).body(PedidoResposta.dePedido(pedidoCriado));
     }
-
-
-
 }
