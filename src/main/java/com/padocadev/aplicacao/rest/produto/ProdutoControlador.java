927fff6a7ffb00029281ead0c8d082b52112ce33
Sonia A. Ribeiro <sonia.ribeiro@alumni.usp.br>
2023-08-19 17:38:21 +0100

    Arruma testes quebrados

--- src/main/java/com/padocadev/aplicacao/rest/produto/ProdutoControlador.java
+++ src/main/java/com/padocadev/aplicacao/rest/produto/ProdutoControlador.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping("/produtos")
